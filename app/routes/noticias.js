21bcde087f270fcf72bfc076e9e4ed62ffd62bce
Rafael <rafacdb@gmail.com>
2017-05-03 11:05:37 -0300

    tentando fazer o constructor da class noticia

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,9 +1,7 @@
-//const pool = require('../../config/database');
-
-module.exports = (app) => {
-  app.get('/noticias', (req, respond) => {
-    const noticiasModel = app.app.models.noticiasModel;
-    const connection = app.config.database;
+module.exports = (application) => {
+  application.get('/noticias', (req, respond) => {
+    const noticiasModel = application.app.models.noticiasModel.Noticias;
+    const connection = application.config.database;
     noticiasModel.getNoticias(connection, (error, result) => {
       respond.render('noticias/noticias', { noticias: result });
     });
